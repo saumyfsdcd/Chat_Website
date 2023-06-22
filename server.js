d08983751902cf9e4c6a0023ddf19e6418fcb4ff
saumyfsdcd <samcode46@gmail.com>
2023-06-22 14:28:37 +0530

    asd

---
+++ server.js
@@ -0,0 +1,27 @@
+const bodyParser = require("body-parser");
+const express=require("express");
+const app=express();
+const http=require("http")
+const server = http.createServer(app);
+// const { Server } = require("socket.io");
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+io.on("connection", (socket)=>{
+    console.log("user connected")
+    socket.on('chat message', (msg)=>{
+        // console.log(msg)
+        io.emit('chat message', msg);
+    })
+})
+const port=process.env.PORT || 3000;
+
+server.listen(port, ()=>{
+    console.log('server started on port 300')
+})
